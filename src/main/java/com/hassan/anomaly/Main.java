b468c71dd207644352374287e1d4fa733c07a99b
hass88-ux <muhammadhassanamir888@gmail.com>
2026-08-20 18:49:22 -0400

    Initial Maven project setup

---
+++ src/main/java/com/hassan/anomaly/Main.java
@@ -0,0 +1,11 @@
+package com.hassan.anomaly;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Setup works");
+
+	}
+
+}
